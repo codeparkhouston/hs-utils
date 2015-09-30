f2156203fb415edeb567e2499ac3ff81148f76ab
pandafulmanda <pandafulmanda@gmail.com>
2015-09-30 01:50:20 -0500

    updates for lesson 3

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,4 +1,6 @@
 window._ = require('lodash');
+window._.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
 var codes;
 if(typeof Prism === 'object'){
   codes = document.querySelectorAll('pre code:not([data-manual-highlight])');
